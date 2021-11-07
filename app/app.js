cbb31c3fd596f6d1973d036dd9b7cefa694b88ff
ChristianIgn <christian.tapia@mail.udp.cl>
2021-11-07 20:10:07 -0300

    contexto en docker-compose y post en app

--- app/app.js
+++ app/app.js
@@ -1,11 +1,38 @@
+const { Kafka } = require('kafkajs')
 const express = require('express')
 const app = express()
 const port = process.env.LISTENING_PORT
 
-app.get('/', (req, res) => {
-    console.log(process.env.KAFKA_BROKER)
-    res.send('Hello World!')
+app.use(express.json());
+
+const kafka = new Kafka({
+    clientId: 'my-app',
+    brokers: ['kafka1:9092', 'kafka2:9092']
 })
+
+const producer = kafka.producer()
+
+app.post('/', (req, res) => {
+    const {id,correo_vendedor,correo} = req.body;
+    
+    await producer.connect();
+    console.log(process.env.KAFKA_BROKER)
+    await producer.send({
+    topic: 'orders',
+    messages: [
+        { 
+            id,
+            correo_vendedor,
+            correo
+         },
+    ],
+    })
+    res.json({
+      msg: "hola",
+    });
+});
+
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
